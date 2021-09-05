461a5b22c1be187128d57f33f96dd291b8a183c2
DreamK0324 <dreamk0324@gmail.com>
2021-09-05 18:49:04 -0400

    Detail optimizing

--- app/src/main/java/com/codepath/flixter/DetailActivity.java
+++ app/src/main/java/com/codepath/flixter/DetailActivity.java
@@ -23,8 +23,9 @@ import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
 
-    private static final String YOUTUBE_API_KEY = "...";
-    public static final String VIDEOS_URL = "...";
+    private static final String YOUTUBE_API_KEY = "AIzaSyBpAB9Cyo_EiHf3EOAYAiTzOd39PjN0w24";
+    public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
 
     TextView tvTitle;
     TextView tvOverview;
